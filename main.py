9f7034a3c2ac9934c104155085889cc1d20609b6
Slobodan Guzvica (E-Search) <a-sguzvica@microsoft.com>
2022-06-13 19:36:44 +0200

    fixing prev commit

--- main.py
+++ main.py
@@ -40,40 +40,6 @@ flappy = Bird(100, int(screenHeight / 2))
 
 birdGroup.add(flappy)
 
-
-class Bird(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        self.index = 0
-        self.counter = 0
-        for num in range(1, 4):
-            img = pygame.image.load('images/redbird-midflap.png')
-            self.images.append(img)
-        self.image = self.images[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.velocity = 0
-        self.clicked = False
-
-    def update(self):
-        self.velocity += 0.5
-
-        if(self.velocity>8):
-            self.velocity=8
-
-        if self.rect.bottom < dataAdapter.groundHeight:
-            self.rect.y += int(self.velocity)
-
-        if(pygame.mouse.get_pressed()[0] == 1 and self.clicked == False) :
-            self.clicked = True
-            self.vel -= 10
-
-        if(pygame.mouse.get_pressed()[0] == 0) :
-            self.clicked = False
-
-
 run = True
 while run:
 
@@ -97,9 +63,7 @@ while run:
         groundScroll -= scrollSpeed
         if abs(groundScroll) > screenWidth:
             groundScroll = 0
-
     
-     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
